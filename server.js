98d57c8a7cacbab1d5d22ee202473cd25dcd52d5
Dzmitry_Belaichuk <dzmitry_belaichuk@epam.com>
2018-08-13 11:43:25 +0300

    Add several main routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+const users = require('./routes/api/users');
+
 const app = express();
 
 // DB Config
@@ -11,8 +15,15 @@ mongoose.connect(db, { useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
+// Main route
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// Use routes
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+app.use('/api/users', users);
+
+// Launch sequence
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server runnning on port ${port}`));
