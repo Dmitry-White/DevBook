9ceb9a69a2ba4c87227a3c52d1415a6290bc7139
Dzmitry_Belaichuk <dzmitry_belaichuk@epam.com>
2018-08-15 15:44:27 +0300

    Add private / profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 // Create custom router
 const router = express.Router();
@@ -6,9 +8,29 @@ const router = express.Router();
 // Load Profile Model
 const Profile = require('../../models/Profile');
 
+// Load User Model
+const User = require('../../models/User');
+
 // @route  GET api/profile/test
 // @desc   Tests profile route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: "Profile Works"}));
+
+// @route  GET api/profile
+// @desc   Get current users profile
+// @access Private
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors)
+            }
+            res.json(profile);
+        })
+        .catch(err => console.log(err));
+});
 
 module.exports = router;
