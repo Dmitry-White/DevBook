2f20a9968dad24cfcf4864399f1bb07617f1ac92
Dmitry <ecio1476@gmail.com>
2018-08-21 17:47:26 +0300

    Add initial redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import './App.css';
+
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route path='/' exact component={Landing} />
-          <div className='container'>
-            <Route path='/register' exact component={Register} />
-            <Route path='/login' exact component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route path='/' exact component={Landing} />
+            <div className='container'>
+              <Route path='/register' exact component={Register} />
+              <Route path='/login' exact component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
