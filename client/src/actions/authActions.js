dac8240b4201cbdd438981b60c24f34bc1067de6
Dmitry <ecio1476@gmail.com>
2018-08-22 22:02:42 +0300

    Add login action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,14 +1,30 @@
 import axios from 'axios';
+
+import setAuthToken from '../utils/setAuthToken';
 import { GET_ERRORS } from './types';
+
+const errObj = err => ({
+    type: GET_ERRORS,
+    payload: err.response.data
+})
 
 // Register
 export const registerUser = (userData, history) => dispatch => {
     axios.post('/api/users/register', userData)
         .then(() => history.push('/login'))
-        .catch(err =>
-            dispatch({
-                type: GET_ERRORS,
-                payload: err.response.data
-            })
-        );
+        .catch(err => dispatch(errObj(err)));
+};
+
+// Login
+export const loginUser = (userData, history) => dispatch => {
+    axios.post('/api/users/login', userData)
+        .then(res => {
+            // Save to LocalStorage
+            const { token } = res.data;
+            localStorage.setItem('jwtToken', token);
+            setAuthToken(token);
+
+            history.push('/dashboard');
+        })
+        .catch(err => dispatch(errObj(err)));
 };
