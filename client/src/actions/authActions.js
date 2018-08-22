4865559574278255f2d79e101b29ec51c3bec093
Dmitry <ecio1476@gmail.com>
2018-08-22 22:32:25 +0300

    Add setting current user state

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,30 +1,42 @@
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 import setAuthToken from '../utils/setAuthToken';
-import { GET_ERRORS } from './types';
-
-const errObj = err => ({
-    type: GET_ERRORS,
-    payload: err.response.data
-})
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register
 export const registerUser = (userData, history) => dispatch => {
     axios.post('/api/users/register', userData)
         .then(() => history.push('/login'))
-        .catch(err => dispatch(errObj(err)));
+        .catch(err => dispatch(setErrors(err)));
 };
 
-// Login
+// Login;
 export const loginUser = (userData, history) => dispatch => {
     axios.post('/api/users/login', userData)
         .then(res => {
             // Save to LocalStorage
             const { token } = res.data;
             localStorage.setItem('jwtToken', token);
+
             setAuthToken(token);
+
+            // Decode token to get data
+            const decoded = jwt_decode(token);
+
+            dispatch(setCurrentUser(decoded));
 
             history.push('/dashboard');
         })
-        .catch(err => dispatch(errObj(err)));
+        .catch(err => dispatch(setErrors(err)));
 };
+
+const setErrors = err => ({
+    type: GET_ERRORS,
+    payload: err.response.data
+});
+
+const setCurrentUser = decoded => ({
+    type: SET_CURRENT_USER,
+    payload: decoded
+})
