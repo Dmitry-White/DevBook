b6023d4f6ad1e4f1f082f87a8b44493543d6e2c4
Dmitry <ecio1476@gmail.com>
2018-08-24 13:32:09 +0300

    Initial create-profile component

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/createProfile/CreateProfile';
 
 import './App.css';
 
@@ -46,6 +47,7 @@ class App extends Component {
               <Route path='/login' exact component={Login} />
               <Switch>
                 <PrivateRoute path='/dashboard' exact component={Dashboard} />
+                <PrivateRoute path='/create-profile' exact component={CreateProfile} />
               </Switch>
             </div>
             <Footer />
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,7 +4,7 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { loginUser } from '../../actions/authActions';
-import TextFieldGroup from '../common/textFieldGroup';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Login extends Component {
     state = {
