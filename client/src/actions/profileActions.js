f627357fdcd753c6fded30a62a2547c848bf50bd
Dmitry <ecio1476@gmail.com>
2018-08-24 19:09:31 +0300

    Add delete account functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, } from './types';
+import {
+    GET_PROFILE,
+    SET_CURRENT_USER,
+    PROFILE_LOADING,
+    GET_ERRORS,
+    CLEAR_CURRENT_PROFILE
+} from './types';
 
 // Current Profile
 export const getCurrentProfile = () => dispatch => {
@@ -24,6 +30,18 @@ export const createProfile = (profileData, history) => dispatch => {
     axios.post('/api/profile', profileData)
         .then(res => history.push('/dashboard'))
         .catch(err => dispatch(setErrors(err)));
+};
+
+// Delete User and Profile
+export const deleteAccount = () => dispatch => {
+    if (window.confirm('Are you sure? This can NOT be undone!')) {
+        axios.delete('/api/profile')
+            .then(res => dispatch({
+                type: SET_CURRENT_USER,
+                payload: {}
+            }))
+            .catch(err => dispatch(setErrors(err)));
+    }
 };
 
 const setErrors = err => ({
