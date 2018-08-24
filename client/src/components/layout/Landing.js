e741dc60a18ba7840ad47d0af353636a7674bc4b
Dmitry <ecio1476@gmail.com>
2018-08-24 10:01:34 +0300

    Add appropriate favicon

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -18,10 +18,10 @@ class Landing extends Component {
                         <div className="row">
                             <div className="col-md-12 text-center">
                                 <h1 className="display-3 mb-4">DevBook</h1>
-                                <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
+                                <p className="lead">Helps you connect and share with the people in your dev life</p>
                                 <hr />
                                 <Link to='/register' className="btn btn-lg btn-info mr-2">Sign Up</Link>
-                                <Link to='/login' className="btn btn-lg btn-light">Login</Link>
+                                <Link to='/login' className="btn btn-lg btn-light">Log In</Link>
                             </div>
                         </div>
                     </div>
