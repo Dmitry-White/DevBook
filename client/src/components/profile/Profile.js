7ecb9908956d110b4f5788d03a3a2daec7837919
Dmitry <ecio1476@gmail.com>
2018-08-26 20:54:02 +0300

    Add profile and about section

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,71 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import ProfileAbout from './ProfileAbout';
+import ProfileCreds from './ProfileCreds';
+import ProfileGithub from './ProfileGithub';
+import ProfileHeader from './ProfileHeader';
+import Spinner from '../common/Spinner';
+import { getProfileByHandle } from '../../actions/profileActions';
+
+class Profile extends Component {
+    componentDidMount() {
+        const handle = this.props.match.params.handle;
+        if (handle) {
+            this.props.getProfileByHandle(handle);
+        }
+    };
+
+    render() {
+        const { profile, loading } = this.props.profile;
+        let profileContent;
+
+        if (profile === null || loading) {
+            profileContent = <Spinner />
+        } else {
+            profileContent = (
+                <Fragment>
+                    <ProfileHeader profile={profile} />
+                    <ProfileAbout profile={profile} />
+                    <ProfileCreds profile={profile} />
+                    <ProfileGithub profile={profile} />
+                </Fragment>
+            );
+        }
+
+        return (
+            <div className="profile">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+
+                            <div className="row">
+                                <div className="col-6">
+                                    <Link to='/profiles' className="btn btn-light mb-3 float-left">
+                                        Back To Profiles
+                                    </Link>
+                                </div>
+                                <div className="col-6"></div>
+                            </div>
+
+                            {profileContent}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    };
+};
+
+Profile.propTypes = {
+    getProfileByHandle: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    profile: state.profile
+});
+
+export default connect(mapStateToProps, { getProfileByHandle })(Profile);
---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import isEmpty from '../../validation/isEmpty';
+
+const ProfileAbout = props => {
+    const { user, bio, skills } = props.profile;
+    const firstName = user.name.trim().split(' ')[0];
+    return (
+        <div className="row">
+            <div className="col-md-12">
+                <div className="card card-body bg-light mb-3">
+                    <h3 className="text-center text-info">{firstName}'s Bio</h3>
+                    <p className="lead">
+                        {isEmpty(bio)
+                            ? <span>{firstName} does not have a bio</span>
+                            : <span>{bio}</span>}
+                    </p>
+                    <hr />
+                    <h3 className="text-center text-info">Skill Set</h3>
+                    <div className="row">
+                        <div className="d-flex flex-wrap justify-content-center align-items-center">
+                            {skills.map((skill, index) => (
+                                <div key={index} className="p-3">
+                                    <i className="fa fa-check" />
+                                    {` ${skill}`}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileAbout;
