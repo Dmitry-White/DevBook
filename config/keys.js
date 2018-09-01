bb1c8d1b956d57a0bcf47b1e9df70e0cd05b93eb
Dmitry <ecio1476@gmail.com>
2018-09-01 16:02:57 +0300

    Prepare to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://DmitryWhite:DmitryWhite78@ds119572.mlab.com:19572/devbook',
-    secretOrKey: 'secret'
+if (process.env.NODE_ENV == 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
